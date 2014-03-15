596466a26d960500a5bae1538fc1aee369a8c4fd
elvisvoer <elvis.adomnica@gmail.com>
2014-03-15 22:41:56 +0100

    clean the code

--- DashBoard/project.js
+++ DashBoard/project.js
@@ -5,49 +5,7 @@ angular.module('project', [ 'device' ]).config(function($routeProvider) {
 	}).otherwise({
 		redirectTo : '/'
 	});
-}).value('RoleList', [ {
-	name : 'admin',
-	value : '0'
-}, {
-	name : 'user',
-	value : '1'
-} ]);
-
-var Token = function() {
-
-};
-Token.prototype.token = "BnFmMHroIllxSDbOaSXmUNF1ddYt9G94VAVnaC0w4GI";
-Token.prototype.getToken = function() {
-	return this.token;
-};
-
-function getCookie(c_name) {
-	var c_value = document.cookie;
-	var c_start = c_value.indexOf(" " + c_name + "=");
-	if (c_start == -1) {
-		c_start = c_value.indexOf(c_name + "=");
-	}
-	if (c_start == -1) {
-		c_value = null;
-	} else {
-		c_start = c_value.indexOf("=", c_start) + 1;
-		var c_end = c_value.indexOf(";", c_start);
-		if (c_end == -1) {
-			c_end = c_value.length;
-		}
-		c_value = unescape(c_value.substring(c_start, c_end));
-	}
-	return c_value;
-}
-
-function setCookie(c_name, value, exdays) {
-	var exdate = new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value = escape(value)
-			+ ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-	document.cookie = c_name + "=" + c_value + ";domain=" + document.domain
-			+ ";path=/";
-}
+});
 
 function IndexCtrl($scope, $location, Device) {
 	
