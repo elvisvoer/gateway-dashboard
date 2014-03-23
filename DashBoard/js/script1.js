d91ed27cf3621229c81e51e0e755c4b7e40c1c70
elvisvoer <elvis.adomnica@gmail.com>
2014-03-23 20:18:59 +0100

    Added multiple reads and pair blocks
    Added read data from all available devices and pair block functionality

--- DashBoard/js/script1.js
+++ DashBoard/js/script1.js
@@ -1,30 +1,21 @@
-window.onload = function(){
-	//temperature initialization
+function PWidget(pw_id) {
+	//elem initialization
 	
-	
-	var temperature = document.getElementById("temperature");
-	var ctxT = temperature.getContext("2d");
+	var elem = document.getElementById(pw_id);
+	var ctxT = elem.getContext("2d");
 	//dimensions
-	var WT = temperature.width;
-	var HT = temperature.height;
+	var WT = elem.width;
+	var HT = elem.height;
 	
-	var humidity = document.getElementById("humidity");
-	var ctxH = humidity.getContext("2d");
-	//dimensions
-	var WH = humidity.width;
-	var HH = humidity.height;
-	
-	//Temperature Variables
-	var temp = 0;
-	var hum = 0;
-	var colorT = "#FF812B";
-	var colorH = "#00FFE3"; 
+	//elem Variables
+	var colorT = "#FF812B"; 
 	var bgcolor = "#E6FFEB";
 	var text;
 	
-	function initT(temp)
+	
+	this.draw = function(value)
 	{
-		//Clear the temperature everytime a chart is drawn
+		//Clear the elem everytime a chart is drawn
 		ctxT.clearRect(0, 0, WT, HT);		
 		//Background 360 degree arc
 		ctxT.beginPath();
@@ -38,7 +29,7 @@ window.onload = function(){
 		ctxT.stroke();
 		//gauge will be a simple arc
 		//Angle in radians = angle in degrees * PI / 180
-		var radians = temp * Math.PI / 25;
+		var radians = value * Math.PI / 25;
 		ctxT.beginPath();
 		ctxT.strokeStyle = colorT;
 		ctxT.lineWidth = 30;
@@ -51,7 +42,7 @@ window.onload = function(){
 		//Lets add the text
 		ctxT.fillStyle = colorT;
 		ctxT.font = "50px bebas";
-		text = Math.floor(temp) + "°";
+		text = Math.floor(value) + "°";
 		//Lets center the text
 		//deducting half of text width from position x
 		text_width = ctxT.measureText(text).width;
@@ -59,49 +50,7 @@ window.onload = function(){
 		//be measured easily. There are hacks but we will keep it manual for now.
 		ctxT.fillText(text, WT/2 - text_width/2, HT/2 + 15);
 	}
-	
-	function initH(hum)
-	{
-		//Clear the temperature everytime a chart is drawn
-		ctxH.clearRect(0, 0, WH, HH);		
-		//Background 360 degree arc
-		ctxH.beginPath();
-		ctxH.strokeStyle = bgcolor;
-		ctxH.lineWidth = 30;
-		ctxH.shadowColor = '#000000';
-		ctxH.shadowBlur = 20;
-		ctxH.shadowOffsetX = -5;
-		ctxH.shadowOffsetY = 2;
-		ctxH.arc(WH/2, HH/2, 100, 0, Math.PI*2, false); //you can see the arc now
-		ctxH.stroke();
-		//gauge will be a simple arc
-		//Angle in radians = angle in degrees * PI / 180
-		var radians = hum * Math.PI * 1.8 / 90 ;
-		ctxH.beginPath();
-		ctxH.strokeStyle = colorH;
-		ctxH.lineWidth = 30;
-		//The arc starts from the rightmost end. If we deduct 90 degrees from the angles
-		//the arc will start from the topmost end
-		ctxH.arc(WH/2, HH/2, 100, 0 - 90*Math.PI/180, radians - 90*Math.PI/180, false); 
-		//you can see the arc now
-		ctxH.stroke();
-		
-		//Lets add the text
-		ctxH.fillStyle = colorH;
-		ctxH.font = "50px bebas";
-		text = Math.floor(hum) + "%";
-		//Lets center the text
-		//deducting half of text width from position x
-		text_width = ctxH.measureText(text).width;
-		//adding manual value to position y since the height of the text cannot
-		//be measured easily. There are hacks but we will keep it manual for now.
-		ctxH.fillText(text, WH/2 - text_width/2, HH/2 + 15);
-	}
 
-	initT(23);
-	
-	initH(90);
-	
-	
+	this.draw(0);
 	
 };
--- DashBoard/project.js
+++ DashBoard/project.js
@@ -1,4 +1,4 @@
-angular.module('project', [ 'device' ]).config(function($routeProvider) {
+var app = angular.module('project', [ 'device', 'blocks' ]).config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller : IndexCtrl,
 		templateUrl : 'main.html'
@@ -7,49 +7,100 @@ angular.module('project', [ 'device' ]).config(function($routeProvider) {
 	});
 });
 
-function IndexCtrl($scope, $location, Device) {
-	
-	//init devices value on 0
-	$scope.temperature = 0;
-	$scope.humidity = 0;
+/*
+app.value("RoundWidget", null)
+   .factory("RoundWidget", function() { 
+		// returns anonymous object with Factory method 'createWidgets' which creates PWidgets
+		return { createWidget: function(id){ return new PWidget(id); } }; 
+});
+*/
 
+// User object 
+var User = { token : 'BnFmMHroIllxSDbOaSXmUNF1ddYt9G94VAVnaC0w4GI' };
 	
-	setInterval( function (){
-		//temperature
-		var d = new Date();
-		var now = d.getTime();
-		var from = now - 120000;
-		Device.get({
-			id : "00000000F6B14876_0_0_1",
-			to : now,
-			from : from
-		}, function(device) {
-			if(device.data.length > 0)
-			{
-				$scope.temperature = device.data[device.data.length - 1].v;
-			}
-		});
+function IndexCtrl($scope, $location, $routeParams, Block, Devices, DeviceData) {
+
+	//var self = this;
+	// query the registered devices
+	 Devices.get(function(d)
+	 {
+		var sensors = []
+		for(var i in d.data)
+	    {
+			// only devices that are sensors and not silent and has time series
+			if(d.data[i].is_sensor == 1 /*&& d.data[i].is_silent == 0*/ && d.data[i].has_time_series)
+				sensors.push( { id: i, data: d.data[i], value: 0 } );
+		}
+		$scope.sensors = sensors;
+		console.log(d);
 		
-		//humidity
-		Device.get({
-			id : "00000000F6B14876_0_0_8",
-			to : now,
-			from : from
-		}, function(device) {
-			if(device.data.length > 0)
-			{
-				$scope.humidity = device.data[device.data.length - 1].v;
-			}
-		});
 		
-	}, 5000);
+		setInterval( function (){
+			//temperature
+			var d = new Date();
+			var now = d.getTime();
+			var from = now - 120000;
+			for(var i in sensors)
+			{
+//				console.log(sensors[i].id);
+				DeviceData.get({
+					id : sensors[i].id,
+					to : now,
+					from : from
+				}, 
+				// closure function for passing the sensor index
+				(function(x){
+					return function(device) {
+						//console.log(x);
+						if(device.data.length > 0)
+						{
+							$scope.sensors[x].value = device.data[device.data.length - 1].v.toFixed(2);
+						}
+						else
+						{
+							$scope.sensors[x].value = 0;
+						}
+					}
+				})(i));
+			}
+		}, 20000);
+	 });
+
+	$scope.getBlocks = function(){
+		
+		Block.get(function(b)
+		{
+			var blocks = [];
+			for(var i in b.data)
+			{
+				blocks.push( { id: i, data: b.data[i] } );
+			}
+			$scope.blocks = blocks;
+			console.log(blocks);
+		});
+	};
 	
+	$scope.unpairBlock = function(id){
+		
+		console.log(id);
+		Block.get({ id: id }, function(b)
+		{
+			console.log(b);
+			b.destroy(id, function() {
+				$scope.getBlocks();
+			});
+		});
+	};
+	
+	/*
+	
+	*/
 }
 
 angular.module('device', [ 'ngResource' ]).factory(
-		'Device',
+		'DeviceData',
 		function($resource) {
-			var user_access_token = 'BnFmMHroIllxSDbOaSXmUNF1ddYt9G94VAVnaC0w4GI';
+			var user_access_token = User.token;
 			var interval = '1min';
 			var Device = $resource('https://api.ninja.is/rest/v0/device/:id/data?fn=mean&from=:from&to=:to&interval=:intv&user_access_token=:token',
 			{
@@ -58,4 +109,33 @@ angular.module('device', [ 'ngResource' ]).factory(
 			});
 
 			return Device;
+		}).factory(
+		'Devices',
+		function($resource) {
+			var user_access_token = User.token;
+			return $resource('https://api.ninja.is/rest/v0/devices?user_access_token=:token',
+			{
+				token : user_access_token,
+			});
+
+			//return Devices;
 		});
+		
+angular.module('blocks', [ 'ngResource' ]).factory(
+		'Block',
+		function($resource) {
+			var user_access_token = User.token;
+			var Block = $resource('https://api.ninja.is/rest/v0/block/:id/?user_access_token=:token', {
+				token : user_access_token,
+			});
+
+			Block.prototype.destroy = function(bid, cb) {
+				console.log(this);
+				return Block.remove({
+					id : bid
+				}, cb);
+			};
+
+			return Block;
+		});
+		
