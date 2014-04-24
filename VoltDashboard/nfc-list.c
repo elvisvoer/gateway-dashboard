2f3509c8544eae23a2b956a74e61575ab5a65925
elvis <elvis@elvis-Lenovo-G580.(none)>
2014-04-24 23:31:30 +0200

    fixed on demand read

--- VoltDashboard/nfc-list.c
+++ VoltDashboard/nfc-list.c
@@ -14,20 +14,62 @@
 #include <string.h>
 
 #include <unistd.h>
+#include <sys/types.h>
 
 #include <nfc/nfc.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #include "nfc-utils.h"
 
 #define MAX_DEVICE_COUNT 2
 #define MAX_TARGET_COUNT 2
 
+int client_socket;
+struct sockaddr_in servaddr,cliaddr;
+struct hostent *server;
+
+void init_socket(char *serverName)
+{
+	client_socket=socket(AF_INET,SOCK_STREAM,0);
+	if (client_socket< 0){
+		perror("ERROR: opening socket\n");
+		exit(0);
+	}
+
+	server = gethostbyname(serverName);
+	if (server == NULL) {
+		perror("ERROR: no such host\n");
+		exit(0);
+	}
+
+	memset(&servaddr,0,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+   	servaddr.sin_addr.s_addr=inet_addr(server->h_name);
+   	servaddr.sin_port=htons(8124);
+	if(connect(client_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+	{	
+		fprintf(stderr, "Cannot connect to socket\n");
+		close(client_socket);
+		exit(1);
+	}
+
+}
+
 
 int
 main(int argc, const char *argv[])
 {
-	(void) argc;
-  
+	int fd[2];
+	char readbuffer[1000];
+	pipe(fd);
+
 	nfc_context *context;
 	nfc_init(&context);
 	
@@ -62,22 +104,26 @@ main(int argc, const char *argv[])
 	  for (i = 0; i < szDeviceFound; i++) {
 	  
 		if ((childpid = fork()) == 0)  // child proccess
-      	        {
-		   nfc_device *pnd;	
-		   while(1){ //infinite loop in child
+      	    	{
+
+		   nfc_device *pnd;
+		   close(fd[0]);
+		   dup2(fd[1], 1);
+//
+//		   while(1)
+//		   { //infinite loop in child
 		   	nfc_target ant[MAX_TARGET_COUNT];
 		   	pnd = nfc_open(context, connstrings[i]);
 			if(pnd == NULL)
 				printf("Unable to open device %s:\n", connstrings[i]);
 	
-		   	nfc_initiator_init(pnd);
+		   //	nfc_initiator_init(pnd);
 		   	nfc_target pant[MAX_TARGET_COUNT];
 
-		
+		while(1){
+			nfc_initiator_init(pnd);
 		   	res = nfc_initiator_list_passive_targets(pnd, nm, ant, MAX_TARGET_COUNT);
-		
 		   	int n;
-		
 		   	do {	
 				for (n = 0; n < res; n++) {	
 				if( memcmp((void*)&pant[n], (void*)&ant[n], sizeof(ant[n])) != 0)
@@ -87,15 +133,36 @@ main(int argc, const char *argv[])
 					printf("\n");
 				    	memcpy((void*)&pant[n], (void*)&ant[n], sizeof(ant[n]));
 				}
-			  }
+			  	}
 		  	} while (nfc_initiator_list_passive_targets(pnd, nm, ant, MAX_TARGET_COUNT) == 0);
+
 //                        nfc_close(pnd);
-                  }
+
+                 }
+		   i++; continue;
 		} //outside fork
-		else { printf("Fork: %d\n",childpid); }
+		else { printf("Fork: %d\n", childpid); }
 		
 	  }//end for
-	  
+	  close(fd[1]);
+	  int nbytes;
+	  init_socket("127.0.0.1");
+	  while(1)
+//	  do
+	  {
+		memset(readbuffer, 0, sizeof(readbuffer));
+	        // Read in a string from the pipe
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+		if(nbytes > 0)
+		{
+			write(client_socket, readbuffer, strlen(readbuffer));
+		}
+//                sprintf(hugebuff, "echo '%s' >/dev/tcp/127.0.0.1/8124", readbuffer);
+//		system(hugebuff);
+		
+	  }
+//	  while(nbytes > 0);
+//*/
 }
  
 
--- VoltDashboard/readData/index.js
+++ VoltDashboard/readData/index.js
@@ -1,23 +1,43 @@
-var util = require('util')
-  , exec = require('child_process').exec,
-    child;
-
+var util = require('util');
 var io = require('socket.io-client'),
-socket = io.connect('localhost', {
+socket = io.connect('192.168.1.176', {
     port: 3700
 });
 socket.on('connect', function () { console.log("socket connected"); });
 
-socket.on('doScan', function (data) {	
-	var intv = setInterval(function() {
 
-	child = exec('top -b -n1| grep Cpu | awk \'{print $2}\'',
-	function (error, stdout, stderr) {
-		stdout.replace(/(\n|\r|\r\n)$/, '');
-		console.log("DATA: " + stdout);
-		clearInterval(intv);
-		socket.emit('send', { UID: stdout, reader: '0' });
-	});
 
-	}, 500);
+var net = require('net');
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.on('data', function(da){
+	var input = da.toString().split('\n'); 
+	var i = 0;
+	var reader, uid;
+        for(i = 0; i<input.length; i++)
+        {
+	   if(input[i].indexOf("number") != -1) //found reader line
+	   {
+		//split line for finding the number
+		var temp = input[i].split(':');
+		// charAt(1) to avoid space
+		reader = temp[1].charAt(1);
+	   }
+	   else if(input[i].indexOf("UID") != -1)
+	   {
+		var temp = input[i].split(':');
+		uid = temp[1];
+	   }
+	}
+	socket.emit('send', { UID: uid, reader: reader });
+	//console.log(da.toString() + 'Here'); 
+  
+  });
 });
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
+
